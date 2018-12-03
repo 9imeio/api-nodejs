ccd64514fb8642d4deb76df2ff8f1d4cd5fa41af
Andressa Ferreira <andressafn@tce.rj.gov.br>
2018-12-03 16:34:56 -0200

    Tratando error 404 e 505

--- src/routes/person.js
+++ src/routes/person.js
@@ -1,8 +1,26 @@
-let express = require ('express');
+let express = require('express');
 let router = express.Router();
 
-router.get('/person', (req, res)=>{
-    res.send('You have requested a person')
+// QueryString => query property on the request object
+// localhost:3000/person?name=Andressa&age=22
+
+router.get('/person', (req, res) => {
+    if (res.query.name) {
+        res.send(`You have requested a person ${req.query.name}`);
+    } else {
+        res.send('You have requested a person')
+    }
 })
+
+//Params property on the request object
+//localhost:3000/person/Andressa
+router.get('/person/:name', (req, res) => {
+
+    res.send(`You have requested a person ${req.params.name}`);
+});
+
+router.get('/error', (req, res) => {
+    throw new Error('This is a forced error.');
+});
 
 module.exports = router;
